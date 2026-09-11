9351ba2e91e7b4478dd13d68853dbb9e2710e3e8
Francois Le Gall <francois.le-gall@converteo.com>
2026-09-11 15:36:47 +0200

    fix scenario 3

--- scenarios/SC003/patches/int_orders.sql
+++ scenarios/SC003/patches/int_orders.sql
@@ -1,16 +1,11 @@
 select
-
     o.order_id,
-
     o.customer_id,
-
+    c.customer_type as customer_segment,
     o.order_date,
-
     o.order_status,
-
     o.channel,
-
     invalid_function_xyz(o.order_id) as broken_col
-
 from {{ ref('stg_orders') }} as o
-
+left join {{ ref('stg_customers') }} as c
+    on o.customer_id = c.customer_id
