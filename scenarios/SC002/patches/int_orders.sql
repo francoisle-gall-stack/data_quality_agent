ba666bf64258764a67524674c7cb82b6534deca2
Francois Le Gall <francois.le-gall@converteo.com>
2026-09-11 10:58:10 +0200

    modif scenario 2

--- scenarios/SC002/patches/int_orders.sql
+++
@@ -1,8 +0,0 @@
-select
-    o.order_id,
-    o.customer_id,
-    c.email,
-    o.order_date
-from {{ ref('stg_orders') }} as o
-left join {{ ref('stg_customers') }} as c
-    on o.customer_id = c.customer_id
