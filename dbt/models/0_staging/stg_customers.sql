7770824507a36c3a9b47ba755e0c24a7f9bac714
Francois Le Gall <francois.le-gall@converteo.com>
2026-09-11 11:32:38 +0200

    fix(dbt): Add email and customer_type columns to stg_customers.sql so they are available for downstream models. [INC-1960B614]

--- dbt/models/0_staging/stg_customers.sql
+++ dbt/models/0_staging/stg_customers.sql
@@ -3,7 +3,7 @@ select
     first_name,
     last_name,
     email,
-    country_code,
     customer_type,
+    country_code,
     cast(signup_date as date) as signup_date
 from {{ source('raw', 'raw_customers') }}
